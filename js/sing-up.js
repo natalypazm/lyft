7778e44f5db61a7caf39e98201a7f7df441060bf
natalypazm <1000104644.rr@gmail.com>
2017-02-17 14:34:34 -0500

    arreglo paises

--- js/sing-up.js
+++ js/sing-up.js
@@ -1,11 +1,40 @@
 function init(){
 	var next = document.getElementById("nextDatos");
 	next.addEventListener('click',onClickNext);
+    var flagTwo=document.getElementById("flagTwo");
+    var codePais=document.getElementById("codePais");
+    var srcLocal=localStorage.getItem('src');
+    var codeLocal=localStorage.getItem('code');
+    flagTwo.src=srcLocal;
+    codePais.textContent=codeLocal;
 }
 function onClickNext(){
-	var tel=document.getElementById("tel-select").value;
+	var tel=document.getElementById("number").value;
 	localStorage.setItem('number',tel);
-}	
+    var regex = /^([0-9])*$/;
+    
+   if(tel==""||(!regex.test(tel))|| tel.length>9) {
+        alert("Campo inválido");
+            return false; 
+          }
+    nextpage();
+    
+    var aleatorio; 
+    aleatorio = Math.round(Math.random() * (100 - 999) + 999);
+    alert('su codigo es : LAB-'+aleatorio);    
+}
+
+
+function nextpage() {
+    
+    location.href='sing-up-datos.html';
+}
+
+
+
+
+
+/*
 function getRandomArbitrary(min, max){
         return Math.random() * (max - min) + min;
 
@@ -20,3 +49,4 @@ function codigo(){
       	var buttonNext= document.getElementById("nextDatos");
     	buttonNext.addEventListener('click', codeGenerado);
 }
+*/
